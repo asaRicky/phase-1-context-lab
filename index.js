b87dc7002bb13646b252d46ffdcdc43089672de6
asaRicky <derrickomondi208@gmail.com>
2023-04-06 14:26:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 /* Your Code Here */
-
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  function createEmployeeRecords(employees) {
+    return employees.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    const timeIn = {
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date,
+    };
+    this.timeInEvents.push(timeIn);
+    return this;
+  }
+  
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    const timeOut = {
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date,
+    };
+    this.timeOutEvents.push(timeOut);
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find((e) => e.date === date);
+    const timeOut = this.timeOutEvents.find((e) => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate.call(this, date);
+    return hours * this.payPerHour;
+  }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -18,6 +64,15 @@ const allWagesFor = function () {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    return payable;
 }
-
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((e) => e.firstName === firstName);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce(
+      (acc, employee) => acc + allWagesFor.call(employee),
+      0
+    );
+  }
